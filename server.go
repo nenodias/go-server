ea03d17ed058be155598201e26d894d0820a5efe
Horácio Dias <horacio.baptista@supportweb.com>
2018-03-14 08:35:29 -0300

    Example of composing POST with a struct

--- server.go
+++ server.go
@@ -5,6 +5,18 @@ import (
 	"net/http"
 )
 
+type Endereco struct {
+	Rua string `json:"rua" xml:"rua" form:"rua" query:"rua"`
+	Numero string `json:"numero" xml:"numero" form:"numero" query:"numero"`
+	Cidade string `json:"cidade" xml:"cidade" form:"cidade" query:"cidade"`
+}
+
+type Usuario struct {
+	Nome  string `json:"nome" xml:"nome" form:"nome" query:"nome"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+	Endereco Endereco `json:"endereco" xml:"endereco" form:"endereco" query:"endereco"`
+}
+
 func main() {
 	// Usar o gin para rodar o projeto com auto reload
 	e := echo.New()
@@ -16,6 +28,13 @@ func main() {
 			nome = "Mundo"
 		}
 		return c.String(http.StatusOK, "Olá " + nome + " seja bem-vindo")
+	})
+	e.POST("/users", func (c echo.Context) error {
+		u := new(Usuario)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		return c.JSON(http.StatusCreated, u) 
 	})
 	e.Logger.Fatal(e.Start(":3001"))
 }
